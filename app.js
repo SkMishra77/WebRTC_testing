d41758de62d078db3ae7bda993d0d13d85d01806
SkMishra77 <sanatkumarmishra02@gmail.com>
2022-12-19 22:27:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app=express()
+let http=require('http').Server(app)
+
+const port= process.env.PORT || 3000
+
+let io=require('socket.io')(http)
+
+app.use(express.static('public'))
+
+http.listen(port,()=>{
+    console.log('listening on',port)
+})
+
+io.on('connection',socket=>{
+    console.log('A User Connected')
+
+    socket.on('create or join',room=>{
+        console.log('create or join to room ', room)
+
+        var clients = io.sockets.adapter.rooms.get(room);
+        
+        var numClients = typeof clients !== "undefined" ? clients.size:0 ;
+        console.log(room,'has',numClients,'clients')
+        
+        if(numClients==0){
+            socket.join(room)
+            clients = io.sockets.adapter.rooms.get(room);
+            numClients=clients.size
+            socket.emit('created',room)
+        }
+        else if(numClients==1){
+            socket.join(room)
+            console.log('cl size',clients.size)
+            socket.emit('joined',room)
+        }
+        else{
+            socket.emit('full',room)
+            console.log("room full")
+        }
+    })
+
+    socket.on('ready',room =>{
+        socket.broadcast.to(room).emit('ready')
+    })
+
+    socket.on('candidate',event=>{
+        socket.broadcast.to(event.room).emit('candidate',event)
+    })
+    socket.on('offer',event=>{
+        socket.broadcast.to(event.room).emit('offer',event.sdp)
+    })
+    socket.on('answer',event=>{
+        socket.broadcast.to(event.room).emit('answer',event.sdp)
+    })
+
+
+})
+
+
---
+++ public/main.js
@@ -0,0 +1,146 @@
+let divSelectRoom=document.getElementById("selectRoom")
+let divConsultingRoom=document.getElementById("consultingRoom")
+let inputRoomNumber=document.getElementById("roomNumber")
+let btnGoRoom=document.getElementById("goRoom")
+let localVideo=document.getElementById("localVideo")
+let remoteVideo=document.getElementById("remoteVideo")
+let inputCallName=document.getElementById("inputCallName")
+
+let roomNumber,localStream,remoteStream,rtcPeerConnection,isCaller,dataChannel
+
+const iceServer = {
+    'iceServer':[
+        {'urls':'stun:stun.services.morzilla.com'},
+        {'urls':'stun:stun.l.google.com:19302'}
+    ]
+}
+
+const streamConstraints ={
+    audio:true,
+    video:true
+}
+
+const socket= io()
+
+btnGoRoom.onclick = ()=>{
+    if(inputRoomNumber.value === ''){
+        alert('plz enter a valid room Id')
+    }
+    else{
+        roomNumber=inputRoomNumber.value
+        socket.emit('create or join',roomNumber)
+        
+        divSelectRoom.style='display:none'
+        divConsultingRoom.style='display:block'
+    }
+}
+
+
+socket.on('created',room=>{
+    navigator.mediaDevices.getUserMedia(streamConstraints)
+        .then(stream=>{
+            localStream=stream
+            localVideo.srcObject=stream
+            isCaller=true
+        })
+        .catch(err=>{
+            alert(`An error occured ${err}`)
+        })
+})
+
+
+socket.on('joined',room=>{
+    navigator.mediaDevices.getUserMedia(streamConstraints)
+        .then(stream=>{
+            localStream=stream
+            localVideo.srcObject=stream
+            socket.emit('ready',roomNumber)
+        })
+        .catch(err=>{
+            alert(`An error occured ${err}`)
+        })
+})
+
+socket.on('ready',()=>{
+    if (isCaller) {
+        rtcPeerConnection=new RTCPeerConnection(iceServer)
+        rtcPeerConnection.onicecandidate=onIceCandidate
+        rtcPeerConnection.ontrack=onAddStream
+        rtcPeerConnection.addTrack(localStream.getTracks()[0],localStream)
+        rtcPeerConnection.addTrack(localStream.getTracks()[1],localStream)
+        rtcPeerConnection.createOffer()
+            .then(sessionDescription=>{
+                rtcPeerConnection.setLocalDescription(sessionDescription)
+                socket.emit('offer',{
+                    type:'offer',
+                    sdp:sessionDescription,
+                    room:roomNumber
+                })
+
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+
+        dataChannel=rtcPeerConnection.createDataChannel(roomNumber)
+        console.log('data Channel',dataChannel)
+    }
+})
+
+
+socket.on('offer',(event)=>{
+    if (!isCaller) {
+        console.log(event)
+        rtcPeerConnection=new RTCPeerConnection(iceServer)
+        rtcPeerConnection.onicecandidate=onIceCandidate
+        rtcPeerConnection.ontrack=onAddStream
+        rtcPeerConnection.addTrack(localStream.getTracks()[0],localStream)
+        rtcPeerConnection.addTrack(localStream.getTracks()[1],localStream)
+        rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event))
+        rtcPeerConnection.createAnswer()
+            .then(sessionDescription=>{
+                rtcPeerConnection.setLocalDescription(sessionDescription)
+                socket.emit('answer',{
+                    type:'answer',
+                    sdp:sessionDescription,
+                    room:roomNumber
+                })
+            })
+            .catch(err=>{
+                console.log(err)
+            })
+    }
+})
+
+socket.on('answer',event=>{
+    console.log(event)
+    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription(event))
+})
+
+socket.on('candidate',event=>{
+    const candidate=new RTCIceCandidate({
+        sdpMLineIndex:event.label,
+        candidate:event.candidate
+    })
+    if(candidate){
+    rtcPeerConnection.addIceCandidate(candidate)
+    }
+})
+
+function onAddStream(event){
+    remoteVideo.srcObject=event.streams[0]
+    remoteStream=event.streams[0]
+}
+
+function onIceCandidate(event){
+    if(event.candidate){
+        console.log('sending ice candidate',event.candidate)
+        socket.emit('candidate',{
+            type:'candidate',
+            label:event.candidate.sdpMLineIndex,
+            id:event.candidate.sdpMid,
+            candidate:event.candidate.candidate,
+            room:roomNumber
+        })
+    }
+}
